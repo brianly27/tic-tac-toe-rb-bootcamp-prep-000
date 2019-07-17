6c8052db3be46f5b580d0d775912e0ac3f991bb0
Brian Ly <brian.kyang.ly@gmail.com>
2019-07-17 17:57:42 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -52,6 +52,17 @@ def turn(board)
   display_board(board)
 end
 
+def turn_count(board)
+  turns = 0
+  board.each |space|
+    if space == "X" || space == "O"
+      turns += 1
+    end
+  end
+  return turns
+end
+
+
 def current_player(board)
   if turn_count(board) %2 == 0
     "X"
